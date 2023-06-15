2560fb25373e719ab36b295bd4239d369047a314
Akn <sibata486@gmail.com>
2023-06-16 01:41:21 +0900

    add / ImageDrawer の画像の描画、上書き処理をメソッドに抽出

--- Assets/Scripts/ScenarioSceneParts/ImageDrawer.cs
+++ Assets/Scripts/ScenarioSceneParts/ImageDrawer.cs
@@ -26,63 +26,12 @@ namespace ScenarioSceneParts
 
             foreach (var order in scenario.ImageOrders)
             {
-                // Canvas の子である ImageContainer に、空のゲームオブジェクトを乗せる。
-                var targetContainer = ImageContainers[order.TargetLayerIndex];
-                var imageSet = new ImageSet();
-
-                var spriteWrappers = new List<SpriteWrapper>();
-                order.Names.ForEach(name =>
-                {
-                    spriteWrappers.Add(!string.IsNullOrEmpty(name) ? resource.ImagesByName[name] : null);
-                });
-
-                // InheritStatus が指定されている場合は、最前面の画像の状態をコピーする
-                if (order.InheritStatus)
-                {
-                    var f = targetContainer.FrontChild;
-                    if (f != null)
-                    {
-                        order.Scale = f.Scale;
-                        order.X = (int)f.Wx;
-                        order.Y = (int)f.Wy;
-                        order.Angle = order.Angle;
-                    }
-                }
-
-                imageSet.Alpha = 0;
-                imageSet.Scale = order.Scale;
-                imageSet.X = order.X;
-                imageSet.Y = order.Y;
-                imageSet.Angle = order.Angle;
-                imageSet.SortingLayerIndex = order.TargetLayerIndex;
-                targetContainer.AddChild(imageSet);
-
-                imageSet.Draw(spriteWrappers);
-
-                if (!string.IsNullOrWhiteSpace(order.MaskImageName))
-                {
-                    // imageSet.SetMask(resource.MaskImagesByName[order.MaskImageName].Sprite);
-                }
+                AddBaseImage(order);
             }
 
             foreach (var order in scenario.DrawOrders)
             {
-                var targetContainer = ImageContainers[order.TargetLayerIndex];
-                var frontImageSet = targetContainer.FrontChild;
-                drawingImageSet = frontImageSet;
-                drawingDepth = order.Depth;
-
-                for (var i = 0; i < order.Names.Count; i++)
-                {
-                    var name = order.Names[i];
-                    if (string.IsNullOrEmpty(name))
-                    {
-                        continue;
-                    }
-
-                    var sw = new SpriteWrapper { Sprite = resource.ImagesByName[name].Sprite };
-                    frontImageSet.SetSprite(sw, i, new Color(1.0f, 1.0f, 1.0f, 0));
-                }
+                DrawImage(order);
             }
         }
 
@@ -109,6 +58,67 @@ namespace ScenarioSceneParts
         public void SetScenario(Scenario scn)
         {
             scenario = scn;
+        }
+
+        public void DrawImage(ImageOrder order)
+        {
+            var targetContainer = ImageContainers[order.TargetLayerIndex];
+            var frontImageSet = targetContainer.FrontChild;
+            drawingImageSet = frontImageSet;
+            drawingDepth = order.Depth;
+
+            for (var i = 0; i < order.Names.Count; i++)
+            {
+                var name = order.Names[i];
+                if (string.IsNullOrEmpty(name))
+                {
+                    continue;
+                }
+
+                var sw = new SpriteWrapper { Sprite = resource.ImagesByName[name].Sprite };
+                frontImageSet.SetSprite(sw, i, new Color(1.0f, 1.0f, 1.0f, 0));
+            }
+        }
+
+        public void AddBaseImage(ImageOrder order)
+        {
+            // Canvas の子である ImageContainer に、空のゲームオブジェクトを乗せる。
+            var targetContainer = ImageContainers[order.TargetLayerIndex];
+            var imageSet = new ImageSet();
+
+            var spriteWrappers = new List<SpriteWrapper>();
+            order.Names.ForEach(name =>
+            {
+                spriteWrappers.Add(!string.IsNullOrEmpty(name) ? resource.ImagesByName[name] : null);
+            });
+
+            // InheritStatus が指定されている場合は、最前面の画像の状態をコピーする
+            if (order.InheritStatus)
+            {
+                var f = targetContainer.FrontChild;
+                if (f != null)
+                {
+                    order.Scale = f.Scale;
+                    order.X = (int)f.Wx;
+                    order.Y = (int)f.Wy;
+                    order.Angle = order.Angle;
+                }
+            }
+
+            imageSet.Alpha = 0;
+            imageSet.Scale = order.Scale;
+            imageSet.X = order.X;
+            imageSet.Y = order.Y;
+            imageSet.Angle = order.Angle;
+            imageSet.SortingLayerIndex = order.TargetLayerIndex;
+            targetContainer.AddChild(imageSet);
+
+            imageSet.Draw(spriteWrappers);
+
+            if (!string.IsNullOrWhiteSpace(order.MaskImageName))
+            {
+                // imageSet.SetMask(resource.MaskImagesByName[order.MaskImageName].Sprite);
+            }
         }
 
         // public void SetUI(UI ui)
