557b30f64684a5204bf9b64de3f1204d13530d38
Akn <sibata486@gmail.com>
2023-06-19 00:12:13 +0900

    test / テストが通るようにコードを修正

--- Assets/Scripts/Loaders/XmlElementConverters/StopElementConverter.cs
+++ Assets/Scripts/Loaders/XmlElementConverters/StopElementConverter.cs
@@ -1,4 +1,6 @@
-﻿namespace Loaders.XmlElementConverters
+﻿using ScenarioSceneParts;
+
+namespace Loaders.XmlElementConverters
 {
     using System;
     using System.Collections.Generic;
@@ -21,46 +23,48 @@
         {
             var tags = xmlElement.Elements(TargetElementName);
 
-            // if (tags.Count() != 0)
-            // {
-            //     foreach (var stopTag in tags)
-            //     {
-            //         var stopOrder = new StopOrder();
+            if (tags.Count() != 0)
+            {
+                foreach (var stopTag in tags)
+                {
+                    var stopOrder = new StopOrder();
 
-            //         if (!stopTag.Attributes().Any(x => x.Name == targetAttribute))
-            //         {
-            //             Log.Add($"<stop> には target 属性が必須です。Index={scenario.Index}");
-            //         }
-            //         else
-            //         {
-            //             if (Enum.TryParse(stopTag.Attribute(targetAttribute).Value, out StoppableSceneParts d))
-            //             {
-            //                 stopOrder.Target = d;
-            //             }
-            //             else
-            //             {
-            //                 Log.Add($"target 属性の変換に失敗。Index={scenario.Index}");
-            //             }
-            //         }
+                    if (!stopTag.Attributes().Any(x => x.Name == targetAttribute))
+                    {
+                        Log.Add($"<stop> には target 属性が必須です。Index={scenario.Index}");
+                    }
+                    else
+                    {
+                        var val = stopTag.Attribute(targetAttribute).Value;
+                        var upperAttValue = val.Substring(0, 1).ToUpper() + val.Substring(1);
+                        if (Enum.TryParse(upperAttValue, out StoppableSceneParts d))
+                        {
+                            stopOrder.Target = d;
+                        }
+                        else
+                        {
+                            Log.Add($"target 属性の変換に失敗。Index={scenario.Index}");
+                        }
+                    }
 
-            //         if (stopTag.Attribute(layerIndexAttribute) != null)
-            //         {
-            //             stopOrder.LayerIndex = int.Parse(stopTag.Attribute(layerIndexAttribute).Value);
-            //         }
+                    if (stopTag.Attribute(layerIndexAttribute) != null)
+                    {
+                        stopOrder.LayerIndex = int.Parse(stopTag.Attribute(layerIndexAttribute).Value);
+                    }
 
-            //         if (stopTag.Attribute(channelAttribute) != null)
-            //         {
-            //             stopOrder.Channel = int.Parse(stopTag.Attribute(channelAttribute).Value);
-            //         }
+                    if (stopTag.Attribute(channelAttribute) != null)
+                    {
+                        stopOrder.Channel = int.Parse(stopTag.Attribute(channelAttribute).Value);
+                    }
 
-            //         if (stopTag.Attribute(nameAttribute) != null)
-            //         {
-            //             stopOrder.Name = stopTag.Attribute(nameAttribute).Value;
-            //         }
+                    if (stopTag.Attribute(nameAttribute) != null)
+                    {
+                        stopOrder.Name = stopTag.Attribute(nameAttribute).Value;
+                    }
 
-            //         scenario.StopOrders.Add(stopOrder);
-            //     }
-            // }
+                    scenario.StopOrders.Add(stopOrder);
+                }
+            }
         }
     }
 }
