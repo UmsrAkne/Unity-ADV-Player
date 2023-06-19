ccb8f70313965f951ed7693f3e019dd87114099e
Akn <sibata486@gmail.com>
2023-06-20 01:35:45 +0900

    add test / DrawElementConverter のテストを追加

---
+++ Assets/Tests/Loaders/XmlElementConverters/DrawElementConverterTest.cs
@@ -0,0 +1,32 @@
+using System.Xml.Linq;
+using Loaders.XmlElementConverters;
+using NUnit.Framework;
+using SceneContents;
+
+namespace Tests.Loaders.XmlElementConverters
+{
+    public class DrawElementConverterTest
+    {
+        [Test]
+        public void Convertのテスト()
+        {
+            var converter = new DrawElementConverter();
+            var scenario = new Scenario();
+
+            const string xmlText = @"<scenario>" +
+                                   @"<draw a=""imgA"" b=""imgB"" c=""imgC"" d="""" depth=""0.5"" />" +
+                                   @"</scenario>";
+
+            var drawElement = XElement.Parse(xmlText);
+            converter.Convert(drawElement, scenario);
+
+            Assert.AreEqual(0, scenario.ImageOrders.Count);
+            Assert.AreEqual("imgA", scenario.DrawOrders[0].Names[0]);
+            Assert.AreEqual("imgB", scenario.DrawOrders[0].Names[1]);
+            Assert.AreEqual("imgC", scenario.DrawOrders[0].Names[2]);
+            Assert.IsTrue(string.IsNullOrEmpty(scenario.DrawOrders[0].Names[3]));
+            Assert.AreEqual(0.5, scenario.DrawOrders[0].Depth);
+            Assert.IsTrue(scenario.DrawOrders[0].IsDrawOrder);
+        }
+    }
+}
