6b49b3dd5fe0a15953ee60127afe1c231706a1a7
Akn <sibata486@gmail.com>
2023-06-20 01:42:48 +0900

    refactoring / DrawElementConverter

--- Assets/Tests/Loaders/XmlElementConverters/DrawElementConverterTest.cs
+++ Assets/Tests/Loaders/XmlElementConverters/DrawElementConverterTest.cs
@@ -14,19 +14,35 @@ namespace Tests.Loaders.XmlElementConverters
             var scenario = new Scenario();
 
             const string xmlText = @"<scenario>" +
-                                   @"<draw a=""imgA"" b=""imgB"" c=""imgC"" d="""" depth=""0.5"" />" +
+                                   @"<draw a=""imgA"" b=""imgB"" c=""imgC"" d="""" depth=""0.5"" targetLayerIndex=""2""/>" +
                                    @"</scenario>";
 
             var drawElement = XElement.Parse(xmlText);
             converter.Convert(drawElement, scenario);
 
-            Assert.AreEqual(0, scenario.ImageOrders.Count);
+            Assert.AreEqual(1, scenario.DrawOrders.Count);
             Assert.AreEqual("imgA", scenario.DrawOrders[0].Names[0]);
             Assert.AreEqual("imgB", scenario.DrawOrders[0].Names[1]);
             Assert.AreEqual("imgC", scenario.DrawOrders[0].Names[2]);
             Assert.IsTrue(string.IsNullOrEmpty(scenario.DrawOrders[0].Names[3]));
             Assert.AreEqual(0.5, scenario.DrawOrders[0].Depth);
             Assert.IsTrue(scenario.DrawOrders[0].IsDrawOrder);
+        }
+
+        [Test]
+        public void 複数生成テスト()
+        {
+            var converter = new DrawElementConverter();
+            var scenario = new Scenario();
+
+            const string xmlText = @"<scenario>" +
+                                   @"<draw a=""imgA"" b=""imgB"" c=""imgC"" d="""" depth=""0.5"" targetLayerIndex=""2""/>" +
+                                   @"<draw a=""imgA"" b=""imgB"" c=""imgC"" d="""" depth=""0.5"" targetLayerIndex=""2""/>" +
+                                   @"</scenario>";
+
+            var drawElement = XElement.Parse(xmlText);
+            converter.Convert(drawElement, scenario);
+            Assert.AreEqual(2, scenario.DrawOrders.Count);
         }
     }
 }
