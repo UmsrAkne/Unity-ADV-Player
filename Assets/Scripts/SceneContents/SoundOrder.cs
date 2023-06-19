912d9fce4dbebe293fa29fd7e0f26cce51006f7c
Akn <sibata486@gmail.com>
2023-06-20 02:24:49 +0900

    add / 効果音の再生命令に転用するためのプロパティを追加

--- Assets/Scripts/SceneContents/SoundOrder.cs
+++ Assets/Scripts/SceneContents/SoundOrder.cs
@@ -6,6 +6,10 @@
 
         public string FileName { get; set; } = string.Empty;
 
+        public int RepeatCount { get; set; }
+
+        public double Volume { get; set; } = 1.0;
+
         public int Channel { get; set; }
 
         public bool StopRequest { get; set; }
