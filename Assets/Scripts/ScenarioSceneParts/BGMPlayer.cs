707303523cc17afdef250081ededef9772a2e392
Akn <sibata486@gmail.com>
2023-06-23 21:17:59 +0900

    add files / サウンド関連の ScenarioSceneParts を追加

---
+++ Assets/Scripts/ScenarioSceneParts/BGMPlayer.cs
@@ -0,0 +1,45 @@
+﻿using Loaders;
+using SceneContents;
+using UnityEngine;
+
+namespace ScenarioSceneParts
+{
+    public class BGMPlayer : IScenarioSceneParts
+    {
+        private AudioSource BGM { get; set; }
+
+        private bool Playing { get; set; }
+
+        private SceneSetting SceneSetting { get; set; }
+
+        public bool NeedExecuteEveryFrame => false;
+
+        public ExecutionPriority Priority => ExecutionPriority.Low;
+
+        public void Execute()
+        {
+            if (!Playing)
+            {
+                BGM.loop = true;
+                BGM.volume = SceneSetting.BGMVolume;
+                BGM.Play();
+                Playing = true;
+            }
+        }
+
+        public void ExecuteEveryFrame()
+        {
+            // throw new System.NotImplementedException();
+        }
+
+        public void SetResource(Resource resource)
+        {
+            BGM = resource.BGMAudioSource;
+            SceneSetting = resource.SceneSetting;
+        }
+
+        public void SetScenario(Scenario scenario)
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/ScenarioSceneParts/BgvPlayer.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SceneContents;
+
+namespace ScenarioSceneParts
+{
+    public class BgvPlayer : IScenarioSceneParts
+    {
+        private Dictionary<string, ISound> bgVoicesByName;
+        private bool mute = true;
+        private ISound playingVoice;
+
+        private List<ISound> voices;
+        private double volume = 1.0;
+
+        public BgvPlayer(VoicePlayer voicePlayer)
+        {
+            VoicePlayer = voicePlayer;
+            VoicePlayer.SoundComplete += VoicePlayerCompleteEventHandler;
+            VoicePlayer.SoundStart += VoicePlayerStartEventHandler;
+        }
+
+        private VoicePlayer VoicePlayer { get; }
+
+        private double Volume
+        {
+            get => volume;
+            set
+            {
+                if (value is >= 0 and <= 1.0)
+                {
+                    if (playingVoice != null)
+                    {
+                        playingVoice.Volume = value;
+                    }
+
+                    volume = value;
+                }
+            }
+        }
+
+        private bool Playing => voices != null && voices.Any();
+
+        public bool NeedExecuteEveryFrame => true;
+
+        public ExecutionPriority Priority => ExecutionPriority.Low;
+
+        public void Execute()
+        {
+        }
+
+        public void ExecuteEveryFrame()
+        {
+            if (!Playing)
+            {
+                return;
+            }
+
+            if (!playingVoice.IsPlaying)
+            {
+                // 再生中の音声が終了したら、リスト上で次の音声があれば再生、なければシャッフルして先頭から再生する。
+                var currentIndex = voices.IndexOf(playingVoice);
+                if (currentIndex == voices.Count - 1)
+                {
+                    voices = voices.OrderBy(_ => Guid.NewGuid()).ToList();
+                    Play(voices.First());
+                }
+                else
+                {
+                    Play(voices[currentIndex + 1]);
+                }
+            }
+
+            if (mute)
+            {
+                return;
+            }
+
+            Volume += 0.03;
+        }
+
+        public void SetScenario(Scenario scenario)
+        {
+            var bgvOrder = scenario.BgvOrders.FirstOrDefault(vo => vo.Channel == VoicePlayer.Channel);
+            if (bgvOrder != null)
+            {
+                playingVoice?.Stop();
+                voices = bgvOrder.FileNames.Select(n => bgVoicesByName[n]).OrderBy(_ => Guid.NewGuid()).ToList();
+                Volume = 0;
+                Play(voices.FirstOrDefault());
+            }
+
+            scenario.StopOrders.ForEach(order =>
+            {
+                if (order.Target is StoppableSceneParts.BackgroundVoice or StoppableSceneParts.Bgv)
+                {
+                    if (order.Channel == VoicePlayer.Channel)
+                    {
+                        mute = true;
+                        Volume = 0;
+                        voices = new List<ISound>();
+                        playingVoice?.Stop();
+                    }
+                }
+            });
+        }
+
+        public void SetResource(Resource resource)
+        {
+            bgVoicesByName = resource.BGVoicesByName;
+        }
+
+        private void VoicePlayerStartEventHandler(object sender, EventArgs e)
+        {
+            mute = true;
+            Volume = 0;
+        }
+
+        private void VoicePlayerCompleteEventHandler(object sender, EventArgs e)
+        {
+            mute = false;
+        }
+
+        private void Play(ISound v)
+        {
+            playingVoice = v;
+            v.Play();
+            v.Volume = Volume;
+        }
+    }
+}
