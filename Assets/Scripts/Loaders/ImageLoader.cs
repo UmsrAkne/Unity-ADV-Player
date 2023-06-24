89c801df9584a37fccc51bc4223bf94841b66fe6
Akn <sibata486@gmail.com>
2023-06-25 01:57:29 +0900

    add / ImageLoader をテストのため、ロード処理のコードをインターフェースに移動

--- Assets/Scripts/Loaders/ImageLoader.cs
+++ Assets/Scripts/Loaders/ImageLoader.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using SceneContents;
-using UnityEngine;
 
 namespace Loaders
 {
@@ -16,6 +14,10 @@ namespace Loaders
         public TargetImageType TargetImageType { get; set; }
 
         public HashSet<string> UsingFileNames { get; set; } = new();
+
+        public IPathListGen PathListGen { private get; set; } = new FilePathListGen();
+
+        public IMaterialGetter MaterialLoader { get; set; } = new MaterialLoader();
 
         private List<SpriteWrapper> Sprites { get; set; } = new();
 
@@ -43,7 +45,7 @@ namespace Loaders
                 Log.Add($"{targetDirectoryPath} が見つかりませんでした");
             }
 
-            GetImageFileLPaths(targetDirectoryPath).ForEach(path =>
+            PathListGen.GetImageFilePaths(targetDirectoryPath).ForEach(path =>
             {
                 var fileName = Path.GetFileName(path);
                 var fileNameWe = Path.GetFileNameWithoutExtension(path);
@@ -51,7 +53,7 @@ namespace Loaders
 
                 if (pathIsUsingFile || TargetImageType != TargetImageType.EventCg)
                 {
-                    var spWrapper = LoadImage(path);
+                    var spWrapper = MaterialLoader.LoadImage(path);
                     Sprites.Add(spWrapper);
                     SpriteDictionary.Add(fileName, spWrapper);
                     SpriteDictionary.Add(fileNameWe, spWrapper);
@@ -70,41 +72,6 @@ namespace Loaders
                 case TargetImageType.UiImage:
                     break;
             }
-        }
-
-        public SpriteWrapper LoadImage(string targetFilePath)
-        {
-            var size = GetImageSize(targetFilePath);
-            var sp = Sprite.Create(ReadTexture(targetFilePath, (int)size.x, (int)size.y),
-                new Rect(0, 0, (int)size.x, (int)size.y), new Vector2(0.5f, 0.5f), 1);
-            return new SpriteWrapper { Sprite = sp, Width = (int)size.x, Height = (int)size.y };
-        }
-
-        private Texture2D ReadTexture(string path, int width, int height)
-        {
-            var bytes = File.ReadAllBytes(path);
-            var texture = new Texture2D(width, height);
-            texture.LoadImage(bytes);
-            texture.filterMode = FilterMode.Point;
-            return texture;
-        }
-
-        private List<string> GetImageFileLPaths(string targetDirectoryPath)
-        {
-            var allFilePaths = new List<string>(Directory.GetFiles(targetDirectoryPath));
-            return allFilePaths.Where(f => Path.GetExtension(f) == ".png" || Path.GetExtension(f) == ".jpg").ToList();
-        }
-
-        private Vector2 GetImageSize(string path)
-        {
-            var fs = new FileStream(path, FileMode.Open, FileAccess.Read);
-            fs.Seek(16, SeekOrigin.Begin);
-            var buf = new byte[8];
-            var _ = fs.Read(buf, 0, 8);
-            fs.Dispose();
-            var width = ((uint)buf[0] << 24) | ((uint)buf[1] << 16) | ((uint)buf[2] << 8) | buf[3];
-            var height = ((uint)buf[4] << 24) | ((uint)buf[5] << 16) | ((uint)buf[6] << 8) | buf[7];
-            return new Vector2(width, height);
         }
     }
 
---
+++ Assets/Scripts/Loaders/MaterialLoader.cs
@@ -0,0 +1,38 @@
+using System.IO;
+using SceneContents;
+using UnityEngine;
+
+namespace Loaders
+{
+    public class MaterialLoader : IMaterialGetter
+    {
+        public SpriteWrapper LoadImage(string path)
+        {
+            var size = GetImageSize(path);
+            var sp = Sprite.Create(ReadTexture(path, (int)size.x, (int)size.y),
+                new Rect(0, 0, (int)size.x, (int)size.y), new Vector2(0.5f, 0.5f), 1);
+            return new SpriteWrapper { Sprite = sp, Width = (int)size.x, Height = (int)size.y };
+        }
+
+        private Texture2D ReadTexture(string path, int width, int height)
+        {
+            var bytes = File.ReadAllBytes(path);
+            var texture = new Texture2D(width, height);
+            texture.LoadImage(bytes);
+            texture.filterMode = FilterMode.Point;
+            return texture;
+        }
+
+        private Vector2 GetImageSize(string path)
+        {
+            var fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            fs.Seek(16, SeekOrigin.Begin);
+            var buf = new byte[8];
+            var _ = fs.Read(buf, 0, 8);
+            fs.Dispose();
+            var width = ((uint)buf[0] << 24) | ((uint)buf[1] << 16) | ((uint)buf[2] << 8) | buf[3];
+            var height = ((uint)buf[4] << 24) | ((uint)buf[5] << 16) | ((uint)buf[6] << 8) | buf[7];
+            return new Vector2(width, height);
+        }
+    }
+}
