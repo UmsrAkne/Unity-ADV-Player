d214820dda097310df7f935a5cf48b53938bc6a4
Akn <sibata486@gmail.com>
2023-07-20 14:51:43 +0900

    add file / BlinkOrder.cs

---
+++ Assets/Scripts/SceneContents/BlinkOrder.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace SceneContents
+{
+    public class BlinkOrder
+    {
+        public string BaseImageName { get; set; } = string.Empty;
+
+        public List<string> Names { get; set; } = new ();
+    }
+}
