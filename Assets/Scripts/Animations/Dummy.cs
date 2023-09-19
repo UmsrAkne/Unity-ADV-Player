93941ed1481365acf254cca6c9565051d17f0933
Akn <sibata486@gmail.com>
2023-09-19 18:42:20 +0900

    add file / 何もしないアニメーションクラスを追加

---
+++ Assets/Scripts/Animations/Dummy.cs
@@ -0,0 +1,56 @@
+using SceneContents;
+
+namespace Animations
+{
+    /// <summary>
+    ///     ダミーアニメーションです。
+    ///     Delay, Duration プロパティのみ動作します。
+    ///     このアニメーションは実行時にカウントダウンを行いますが、外部に影響を及ぼす動作は何も行いません。
+    ///     アニメーション間のインターバルや、テストに使用します。
+    /// </summary>
+    public class Dummy : IAnimation
+    {
+        public string AnimationName => "dummy";
+
+        public bool IsWorking { get; private set; } = true;
+
+        public IDisplayObject Target { get; set; }
+
+        public ImageContainer TargetContainer { get; set; }
+
+        public int TargetLayerIndex { get; set; }
+
+        public int RepeatCount { get; set; }
+
+        public int Delay { get; set; }
+
+        public int Interval { get; set; }
+
+        public int Duration { get; set; }
+
+        public string GroupName { get; set; }
+
+        public void Execute()
+        {
+            if (Delay-- > 0)
+            {
+                return;
+            }
+
+            if (Duration-- <= 0)
+            {
+                Stop();
+            }
+        }
+
+        public void Start()
+        {
+        }
+
+        public void Stop()
+        {
+            Duration = 0;
+            IsWorking = false;
+        }
+    }
+}
