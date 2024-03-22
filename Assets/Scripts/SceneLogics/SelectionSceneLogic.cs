ea1f677451b19dc30a8ef5da2c7ecd7479882fe2
Akn <sibata486@gmail.com>
2024-03-22 17:39:50 +0900

    add / 各シナリオが入っているディレクトリのパス一覧を取得するコードを実装

--- Assets/Scripts/SceneLogics/SelectionSceneLogic.cs
+++ Assets/Scripts/SceneLogics/SelectionSceneLogic.cs
@@ -1,12 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEngine;
 
 namespace SceneLogics
 {
     public class SelectionSceneLogic : MonoBehaviour
     {
+        private int cursorIndex;
+        private List<string> selectableScenePaths = new ();
+
+        private int CursorIndex
+        {
+            get => cursorIndex;
+            set => cursorIndex = Math.Max(0, Math.Min(value, selectableScenePaths.Count - 1));
+        }
+
         private void Start()
         {
             Debug.Log("SelectionScene に入りました");
+
+            // Application.dataPath は Assets フォルダが取得される。
+            // 各シナリオは、一段上のディレクトリの中の scenes に入っている。
+            var scenesPath = new DirectoryInfo(Application.dataPath).Parent + @"\scenes";
+
+            selectableScenePaths = GetDirectories(scenesPath);
+
+            foreach (var p in selectableScenePaths)
+            {
+                Debug.Log(p);
+            }
+        }
+
+        private List<string> GetDirectories(string path)
+        {
+            var directories = new List<string>();
+
+            try
+            {
+                // 指定したディレクトリ内のディレクトリ一覧を取得
+                directories = Directory.GetDirectories(path).ToList();
+            }
+            catch (System.Exception e)
+            {
+                // エラーが発生した場合はエラーメッセージをログに出力
+                Debug.LogError("Error getting directories: " + e.Message);
+            }
+
+            return directories;
         }
     }
 }
