ba43ef66688b66b899c794ebc8f9b4107bf55a9d
Akn <sibata486@gmail.com>
2024-06-01 18:19:49 +0900

    add / シーンのサムネをマスクされた状態で表示する処理を実装

--- Assets/Scripts/SceneLogics/SelectionSceneLogic.cs
+++ Assets/Scripts/SceneLogics/SelectionSceneLogic.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Loaders;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -23,27 +24,44 @@ namespace SceneLogics
         {
             DebugTools.Logger.Add("SelectionScene に入りました");
 
+            var mainCamera = Camera.main;
+
+            // カメラのビューポート座標 (0, 0.5) は画面の左端の中央を指します
+            var leftEdgePosition = mainCamera.ViewportToWorldPoint(new Vector3(0, 1, mainCamera.nearClipPlane));
+
             // Application.dataPath は Assets フォルダが取得される。
             // 各シナリオは、一段上のディレクトリの中の scenes に入っている。
             var scenesPath = new DirectoryInfo(Application.dataPath).Parent + @"\scenes";
 
             selectableScenePaths = GetDirectories(scenesPath);
+            var materialLoader = new MaterialLoader();
 
-            var cnt = 1;
-            foreach (var p in selectableScenePaths)
+            for (var i = 0; i < selectableScenePaths.Count; i++)
             {
+                var p = selectableScenePaths[i];
                 // UIテキストをインスタンス化し、リストの内容を表示する
                 var sceneDetail = Instantiate(sceneDetailPrefab, gameObject.transform);
+                sceneDetail.transform.position = new Vector3(0, 0, i);
+
                 var t = sceneDetail.GetComponentInChildren<Text>();
                 if (t != null)
                 {
                     t.text = p;
                     var r = t.GetComponent<RectTransform>();
-                    r.anchoredPosition = new Vector2(400, -40 * cnt);
+                    r.anchoredPosition = new Vector2(400, -40 * i);
                     r.sizeDelta = new Vector2(800, r.sizeDelta.y);
                 }
 
-                cnt++;
+                var ren = sceneDetail.GetComponentInChildren<SpriteRenderer>();
+                var spw = materialLoader.LoadImage($@"{p}\images\A0101.png");
+                ren.sprite = spw.Sprite;
+                var scale = 320.0f / spw.Width;
+                var transform1 = ren.transform;
+                transform1.localScale = new Vector3(scale, scale, 1.0f);
+
+                // オブジェクトの位置を設定
+                var pos = new Vector2(leftEdgePosition.x + 160, leftEdgePosition.y - (180 * i));
+                transform1.position = new Vector3(pos.x, pos.y, 1.0f);
             }
         }
 
